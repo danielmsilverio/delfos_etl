43678a0acd554af333d80af2118d089128ecf6cc
Daniel Silvério <danielmsilverio@gmail.com>
2025-12-24 01:15:20 -0300

    add ruff and tasks

--- alvo/app/main.py
+++ alvo/app/main.py
@@ -2,10 +2,11 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
+
+@app.get('/')
 def main():
-    print("Hello from alvo!")
+    print('Hello from alvo!')
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
